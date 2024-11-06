57907d69c616adfe837b23c554a3d7c57f301822
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-11-06 16:42:44 +0000

    Flexbox Mandatory Task 10

--- flexbox/cp.sh
+++ flexbox/cp.sh
@@ -1,7 +1,7 @@
 
-cp "$1-index.html" "$2-index.html"
+cp "$1-article.html" "$2-article.html"
 cp "$1-styles.css" "$2-styles.css"
 
-code "$2-index.html"
+code "$2-article.html"
 code "$2-styles.css"
 
